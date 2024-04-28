05ad7afebe61171e77ac70fe34250d6f9247ef6d
ASALEEMKHAN <asaleemkhan386@gmail.com>
2024-04-28 17:02:24 +0530

    React-first-code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const data = [
+    {
+      image:
+        "https://iheartcraftythings.com/wp-content/uploads/2021/04/How-to-Draw-Cartoon-Face-%E2%80%93-Featured-Image.jpg",
+      name: "Sanjay",
+      batch: "B43WE",
+    },
+    {
+      image:
+        "https://iheartcraftythings.com/wp-content/uploads/2021/04/How-to-Draw-Cartoon-Face-%E2%80%93-Featured-Image.jpg",
+      name: "Surya",
+      batch: "B42WE",
+    },
+    {
+      image:
+        "https://iheartcraftythings.com/wp-content/uploads/2021/04/How-to-Draw-Cartoon-Face-%E2%80%93-Featured-Image.jpg",
+      name: "Jagan",
+      batch: "B41WE",
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.map((person, index) => (
+        <Home
+          key={index}
+          image={person.image}
+          name={person.name}
+          batch={person.batch}
+        />
+      ))}
     </div>
   );
 }
 
 export default App;
+
+// jsx javascript xml
+
+function Home(props) {
+  return (
+    <div className="home-component">
+      <img src={props.image} alt={props.name} />
+      <h2>{props.name}</h2>
+      <h4>{props.batch}</h4>
+    </div>
+  );
+}
+
+/*
+const props = {
+  name : "sanjay",
+  batch : "B43WE"
+}
+props.name
+
+background work:-
+return React.createElement(
+  "div",
+  { id: "home", className: "home-class" },
+  React.createElement("h1", null, "Hello")
+);
+
+*/
+
+/*
+Topic:-
+Why react
+Setup react project
+Understanding JSX
+Components (simple HTML layout to reusable react components)
+Understanding the components
+Passing data through props
+Deploying react to netlify
+*/
